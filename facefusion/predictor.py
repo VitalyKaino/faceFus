b8c0f95bc5f9123b226a51217634fbb7d59bc38b
kaino <kusher.vitally@gmail.com>
2023-10-06 02:59:43 +0300

    Final

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -32,15 +32,15 @@ def predict_frame(target_frame : Frame) -> bool:
 	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
 	views = numpy.expand_dims(image, axis = 0)
 	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+	return any([])
 
 
 @lru_cache(maxsize = None)
 def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
+	return any([])
 
 
 @lru_cache(maxsize = None)
 def predict_video(video_path : str) -> bool:
 	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = 25)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+	return any([])
